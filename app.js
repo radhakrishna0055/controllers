a9ab261856bc3b25fc573b545e8cb212f44ea270
radhakrishna <radhakrishnau55@gmail.com>
2020-02-15 12:18:13 +0530

    connecting webserver to data server

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express=require('express');
-
+const dotenv=require('dotenv');
+const mongoose=require('mongoose');
 const app=express();
-
+const morgan=require('morgan');
 const postRouters=require('./routers/post');
 
+dotenv.config();
 
+mongoose
+.connect(process.env.MONGO_URI,{useNewUrlParser:true} )
+    .then(()=>console.log("DB Connected"));
+
+mongoose.connection.on("error",err=>{
+    console.log(`DB connection error:${err.message}`);
+});
+
+app.use(morgan("dev"));
 app.use("/",postRouters);
 
-app.listen(4000);
+const port=8080;
+app.listen(port,()=>{
+    console.log(`A Node Js API is listening on port:${port}`);
+});
+
